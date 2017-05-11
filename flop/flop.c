8f71c72c4ab538155fbf83c81225b4625bd393b6
Marshall Ward <marshall.ward@gmail.com>
2017-05-11 11:15:07 +1000

    Pthread implementation of avx_add and avx_mac
    This is a first attempt at a pthread implementation of the avx_add and
    avx_mac speed tests.  No attempt is made to sync the calls, nor to
    correctly place them across CPUs.  But the threads are more or less
    working.

--- flop/flop.c
+++ flop/flop.c
@@ -5,34 +5,76 @@
 #include <stdio.h>      /* printf */
 #include <time.h>       /* timespec, clock_gettime */
 
+// pthread testing
+#include <pthread.h>
+#define N_THREADS 1
+
 #include "timer.h"
 #include "avx.h"
 
 
+void * avx_add_thread(void *tid)
+{
+    double runtime;
+
+    runtime = avx_add();
+
+    printf("Thread %ld avx_add runtime: %.12f\n", (long) tid, runtime);
+    /* (iterations) * (8 flops / register) * (8 registers / iteration) */
+    printf("Thread %ld avx_add gflops: %.12f\n",N * 8 * 8 / (runtime * 1e9));
+
+    pthread_exit(NULL);
+}
+
+
+void * avx_mac_thread(void *tid)
+{
+    double runtime;
+
+    runtime = avx_mac();
+
+    printf("Thread %ld avx_mac runtime: %.12f\n", (long) tid, runtime);
+    /* (iterations) * (8 flops / register) * (24 registers / iteration) */
+    printf("Thread %ld avx_mac gflops: %.12f\n",
+            (long) tid, N * 8 * 24 / (runtime * 1e9));
+
+    pthread_exit(NULL);
+}
+
+
 int main(int argc, char *argv[])
 {
-    #pragma omp parallel
-    {
-        double runtime;
+    double runtime;
 
-        runtime = avx_add();
+    /* pthread implementation */
+    pthread_t threads[N_THREADS];
+    pthread_attr_t attr;
+    void *status;
+    long t;
 
-        printf("avx_add\n");
-        printf("-------\n");
-        printf("runtime: %.12f\n", runtime);
-        /* (iterations) * (8 flops / register) * (8 registers / iteration) */
-        printf("gflops: %.12f\n", N * 8 * 8 / (runtime * 1e9));
+    // "The final draft of the POSIX standard specifies that threads should be
+    // created as joinable."  But we specify it anyway.
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-        printf("\n");
+    /* avx_add */
 
-        runtime = avx_mac();
-
-        printf("avx_mac\n");
-        printf("-------\n");
-        printf("runtime: %.12f\n", runtime);
-        /* (iterations) * (8 flops / register) * (24 registers / iteration) */
-        printf("gflops: %.12f\n", N * 8 * 24 / (runtime * 1e9));
+    for (t = 0; t < N_THREADS; t++) {
+        pthread_create(&threads[t], &attr, avx_add_thread, (void *) t);
     }
 
-    return 0;
+    for (t = 0; t < N_THREADS; t++)
+        pthread_join(threads[t], &status);
+
+    /* avx_mac */
+
+    for (t = 0; t < N_THREADS; t++) {
+        pthread_create(&threads[t], &attr, avx_mac_thread, (void *) t);
+    }
+
+    for (t = 0; t < N_THREADS; t++)
+        pthread_join(threads[t], &status);
+
+    pthread_attr_destroy(&attr);
+    pthread_exit(NULL);
 }
