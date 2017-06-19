4b19b24845c013af4f137bb04f641865b56c251a
Marshall Ward <marshall.ward@gmail.com>
2017-06-19 19:21:26 +1000

    Forgot the generic subroutines

---
+++ flop/generic/stopwatch_tsc.c
@@ -0,0 +1,18 @@
+#include <stdint.h>     /* uint64_t */
+#include <stdlib.h>     /* malloc, free */
+#include <time.h>       /* clock[id]_* */
+#include <stdio.h>
+
+#include "stopwatch.h"
+#include "stopwatch_tsc.h"
+
+void stopwatch_init_tsc(Stopwatch *t) 
+{
+    printf("ERROR: TSC timer not supported.\n");
+    exit(-1);
+}
+
+void stopwatch_start_tsc(Stopwatch *t) {}
+void stopwatch_stop_tsc(Stopwatch *t) {}
+double stopwatch_runtime_tsc(Stopwatch *t) { return -1; }
+void stopwatch_destroy_tsc(Stopwatch *t) {};
