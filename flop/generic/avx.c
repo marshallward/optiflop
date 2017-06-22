f6f1df82d982e0734b74290a2b6adc9bc40330c4
Marshall Ward <marshall.ward@gmail.com>
2017-06-22 16:14:46 +1000

    PowerPC build; generic avx result fix
    Generic AVX output was occasionaly garbage due to lack of
    initialisation.  This is now fixed.
    
    Also, some basic rules for PowerPC were added, although there is
    certainly room for improvement here.

--- flop/generic/avx.c
+++ flop/generic/avx.c
@@ -1,12 +1,32 @@
 #include <pthread.h>    /* pthread_* */
 
+#include "bench.h"
+
 void * avx_add(void *args_in)
 {
+    struct thread_args *args;
+
+    args = (struct thread_args *) args_in;
+
+    args->runtime = 0.;
+    args->flops = 0.;
+    args->bw_load = 0.;
+    args->bw_store = 0.;
+
     pthread_exit(NULL);
 }
 
 
 void * avx_mac(void *args_in)
 {
+    struct thread_args *args;
+
+    args = (struct thread_args *) args_in;
+
+    args->runtime = 0.;
+    args->flops = 0.;
+    args->bw_load = 0.;
+    args->bw_store = 0.;
+
     pthread_exit(NULL);
 }
