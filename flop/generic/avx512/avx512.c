4711799f7570da7da6bba105e7d0e4752bfe8e94
Marshall Ward <marshall.ward@gmail.com>
2017-06-27 13:31:49 +1000

    Added avx512_add dummy method

--- flop/generic/avx512/avx512.c
+++ flop/generic/avx512/avx512.c
@@ -2,6 +2,22 @@
 
 #include "bench.h"
 
+
+void * avx512_add(void *args_in)
+{
+    struct thread_args *args;
+
+    args = (struct thread_args *) args_in;
+
+    args->runtime = 0.;
+    args->flops = 0.;
+    args->bw_load = 0.;
+    args->bw_store = 0.;
+
+    pthread_exit(NULL);
+}
+
+
 void * avx512_fma(void *args_in)
 {
     struct thread_args *args;
