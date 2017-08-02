0da65eee439d9936fc92ec1e7f6b40b647c7d02c
Marshall Ward <marshall.ward@gmail.com>
2017-08-02 21:53:00 +1000

    Explicit roofline functions with inline kernels
    Roofline tests are now explicitly coded, rather than relying on
    preprocessor tricks.  All functions are now defined within the roof.c
    file.
    
    Kernels appear to be inlined in gcc, but this may not be true in other
    compilers.

--- flop/bench.h
+++ flop/bench.h
@@ -29,6 +29,8 @@ struct roof_args {
 typedef void (*roof_ptr_t) (int, float, float, float *, float *,
                             struct roof_args *);
 
+typedef void (*kernel_ptr_t) (int, float, float, float *, float *);
+
 struct thread_args {
     /* Input */
     int tid;
--- flop/roof.c
+++ flop/roof.c
@@ -50,56 +50,153 @@ void * roof_thread(void *args_in)
 }
 
 
-/* Note for roof_copy:
- * Many compilers (gcc, icc) will ignore this loop and use its builtin memcpy
- * function, which can perform worse than vectorised loops.
+void roof_kernel(int n, float a, float b,
+                 float * restrict x_in, float * restrict y_in,
+                 struct roof_args *args, kernel_ptr_t kernel,
+                 int flops, int load_bytes, int store_bytes, int offset)
+{
+    float *x, *y;
+
+    Stopwatch *t;
+
+    long r, r_max;
+    int i;
+    double runtime;
+
+    x = ASSUME_ALIGNED(x_in);
+    y = ASSUME_ALIGNED(y_in);
+
+    t = stopwatch_create(TIMER_POSIX);
+
+    r_max = 1;
+    runtime_flag = 0;
+    do {
+        pthread_barrier_wait(&timer_barrier);
+        t->start(t);
+        for (r = 0; r < r_max; r++) {
+            for (i = offset; i < n; i++)
+                kernel(i, a, b, x, y);
+            // Create an impossible branch to prevent loop interchange
+            if (y[0] < 0.) dummy(a, b, x, y);
+        }
+        t->stop(t);
+        runtime = t->runtime(t);
+
+        /* Set runtime flag if any thread exceeds runtime limit */
+        if (runtime > (args->min_runtime)) {
+            pthread_mutex_lock(&runtime_mutex);
+            runtime_flag = 1;
+            pthread_mutex_unlock(&runtime_mutex);
+        }
+
+        pthread_barrier_wait(&timer_barrier);
+        if (!runtime_flag) r_max *= 2;
+
+    } while (!runtime_flag);
+
+    args->runtime = runtime;
+    args->flops = flops * (n - offset) * r_max / runtime;
+    args->bw_load = load_bytes * (n - offset) * sizeof(float) * r_max
+                        / runtime;
+    args->bw_store = store_bytes * (n - offset) * sizeof(float) * r_max
+                        / runtime;
+
+    /* Cleanup */
+    t->destroy(t);
+}
+
+
+/* roof_copy */
+
+/* NOTE: Many compilers (gcc, icc) will ignore the loop and use its builtin
+ * memcpy function, which can perform worse or better than vectorised
+ * loops.
  *
- * To avoid this issue, make sure to disable builtins (usually `-fno-builtin`).
+ * If you want to use SIMD vectorization, then disable builtins
+ * (usually with `-fno-builtin`).
  */
 
-#define ROOF_TEST roof_copy
-#define ROOF_KERNEL y[i] = x[i]
-#define ROOF_FLOPS 0
-#define ROOF_BW_LOAD 1
-#define ROOF_BW_STORE 1
-#include "roof.inc"
+void copy_kernel(int i, float a, float b, float *x, float *y) {
+    y[i] = x[i];
+}
+
+void roof_copy(int n, float a, float b,
+               float * restrict x_in, float * restrict y_in,
+               struct roof_args *args)
+{
+    roof_kernel(n, a, b, x_in, y_in, args, copy_kernel, 0, 1, 1, 0);
+}
 
 
-#define ROOF_TEST roof_ax
-#define ROOF_KERNEL y[i] = a * x[i]
-#define ROOF_FLOPS 1
-#define ROOF_BW_LOAD 1
-#define ROOF_BW_STORE 1
-#include "roof.inc"
+/* roof_ax */
+
+void ax_kernel(int i, float a, float b, float *x, float *y) {
+    y[i] = a * x[i];
+}
+
+void roof_ax(int n, float a, float b,
+             float * restrict x_in, float * restrict y_in,
+             struct roof_args *args)
+{
+    roof_kernel(n, a, b, x_in, y_in, args, ax_kernel, 1, 1, 1, 0);
+}
 
 
-#define ROOF_TEST roof_xpy
-#define ROOF_KERNEL y[i] = x[i] + y[i]
-#define ROOF_FLOPS 1
-#define ROOF_BW_LOAD 2
-#define ROOF_BW_STORE 1
-#include "roof.inc"
+/* roof_xpy */
+
+void xpy_kernel(int i, float a, float b, float *x, float *y)
+{
+    y[i] = x[i] + y[i];
+}
+
+void roof_xpy(int n, float a, float b,
+              float * restrict x_in, float * restrict y_in,
+              struct roof_args *args)
+{
+    roof_kernel(n, a, b, x_in, y_in, args, xpy_kernel, 1, 2, 1, 0);
+}
 
 
-#define ROOF_TEST roof_axpy
-#define ROOF_KERNEL y[i] = a * x[i] + y[i]
-#define ROOF_FLOPS 2
-#define ROOF_BW_LOAD 2
-#define ROOF_BW_STORE 1
-#include "roof.inc"
+/* roof_axpy */
+
+void axpy_kernel(int i, float a, float b, float *x, float *y)
+{
+    y[i] = a * x[i] + y[i];
+}
+
+void roof_axpy(int n, float a, float b,
+               float * restrict x_in, float * restrict y_in,
+               struct roof_args *args)
+{
+    roof_kernel(n, a, b, x_in, y_in, args, axpy_kernel, 2, 2, 1, 0);
+}
 
 
-#define ROOF_TEST roof_axpby
-#define ROOF_KERNEL y[i] = a * x[i] + b * y[i]
-#define ROOF_FLOPS 3
-#define ROOF_BW_LOAD 2
-#define ROOF_BW_STORE 1
-#include "roof.inc"
+/* roof_axpby */
 
-#define ROOF_TEST roof_diff
-#define ROOF_KERNEL y[i] = x[i] - x[i-1]
-#define ROOF_FLOPS 1
-#define ROOF_BW_LOAD 2
-#define ROOF_BW_STORE 1
-#define ROOF_OFFSET 1
-#include "roof.inc"
+void axpby_kernel(int i, float a, float b, float *x, float *y)
+{
+    y[i] = a * x[i] + b * y[i];
+}
+
+
+void roof_axpby(int n, float a, float b,
+                float * restrict x_in, float * restrict y_in,
+                struct roof_args *args)
+{
+    roof_kernel(n, a, b, x_in, y_in, args, axpby_kernel, 3, 2, 1, 0);
+}
+
+
+void diff_kernel(int i, float a, float b, float *x, float *y)
+{
+    y[i] = x[i] - x[i - 1];
+}
+
+
+void roof_diff(int n, float a, float b,
+               float * restrict x_in, float * restrict y_in,
+               struct roof_args *args)
+{
+    roof_kernel(n, a, b, x_in, y_in, args, diff_kernel, 1, 2, 1, 1);
+}
