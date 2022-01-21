ccd6986dc8d2f3152eb64dc07681b87db23e42ad
Marshall Ward <marshall.ward@noaa.gov>
2022-01-20 19:40:23 -0500

    Mem-bound GPU ax+y loop
    This tweaks the "gpy_axpy" test to allow multiple calculations per
    thread, and a bit more control (and automation) for thread/block
    decomposition.
    
    The CUDA GPU tests are now (more or less) fully integrated into the
    standard build, with a few conditional swaps to select CC or NVCC when
    necessary.
    
    The build is slightly more autoconf-aware, although the most obvious
    tests (like looking for nvcc) still do not exist.
    
    I find that for very high array sizes, the best performance happens with
    2 cores per thread (?) and 800 threads per block.  This divides the
    problem into N/1600 blocks.  (Overflow is handled by truncating the
    do-loop in the kernel.  I guess many get truncated to zero iterations?)
    
    Also some of the GPU-local timer stuff print was turned off.  Not sure
    whether to pursue that.  Numbers are great, but I think I am just
    reading one of the warps, and not some synethisized result (e.g. max
    over all warps/blocks/etc).
    
    Also some skeletal code to do timings on the GPU, rather than around the
    kernel submissions.  But that is not quite ready.
    
    Finally, this breaks out the ax+y gpu tests into kernels.  Only the add
    kernel is currently used.

---
+++ src/cuda/gpu_simd.cu
@@ -0,0 +1,129 @@
+#include "roof.h"
+
+/* TODO: V100-specific numbers; how to generalize? */
+#define NCORES 32
+#define NBLOCKS 160
+#define NTHREADS 128
+
+
+__global__ void kadd(long r_max, float *sum, double *runtime)
+{
+    const float eps = 1e-6f;
+    float reg[NCORES];
+    long r;
+    int i;
+
+    long long int start, end;
+
+    for (i = 0; i < NCORES; i++)
+        reg[i] = 1.f;
+
+    start = clock64();
+    for (r = 0; r < r_max; r++)
+        for (i = 0; i < NCORES; i++)
+            reg[i] = reg[i] + eps;
+    end = clock64();
+
+    *runtime = (double) (end - start) / 1.230e9;
+
+    *sum = 0.f;
+    for (i = 0; i < NCORES; i++) *sum = *sum + reg[i];
+}
+
+
+__global__ void kmul(long r_max, float *sum)
+{
+    const float alpha = 1.f + 1e-6f;
+    float reg[NCORES];
+    long r;
+    int i;
+
+    for (i = 0; i < NCORES; i++)
+        reg[i] = 1.f;
+
+    for (r = 0; r < r_max; r++)
+        for (i = 0; i < NCORES; i++)
+            reg[i] = reg[i] * alpha;
+
+    *sum = 0.f;
+    for (i = 0; i < NCORES; i++) *sum = *sum + reg[i];
+}
+
+
+__global__ void kfma(long r_max, float *sum)
+{
+    const float eps = 1e-6f;
+    const float alpha = 1.f + 1e-6f;
+    float reg[NCORES];
+    long r;
+    int i;
+
+    for (i = 0; i < NCORES; i++)
+        reg[i] = 1.f;
+
+    for (r = 0; r < r_max; r++)
+        for (i = 0; i < NCORES; i++)
+            reg[i] = alpha * reg[i] + eps;
+
+    *sum = 0.f;
+    for (i = 0; i < NCORES; i++) *sum = *sum + reg[i];
+}
+
+
+extern "C"
+void gpu_add(void *args_in)
+{
+    struct roof_args *args;     // args
+    cudaEvent_t start, stop;
+    long r_max;
+    float sum, *gpu_sum;
+    float msec, runtime;
+
+    // testing
+    double *gpu_runtime;
+    double new_runtime;
+
+    args = (struct roof_args *) args_in;
+
+    // TODO: useful?
+    //cudaDeviceReset();
+
+    r_max = 1;
+    cudaMalloc(&gpu_sum, sizeof(float));
+    cudaMalloc(&gpu_runtime, sizeof(double));
+
+    /* TODO: Move timer to kernel and use clock64() */
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    *(args->runtime_flag) = 0;
+    do {
+        cudaEventRecord(start);
+        kadd<<<NBLOCKS,NTHREADS>>>(r_max, gpu_sum, gpu_runtime);
+        cudaEventRecord(stop);
+
+        // Get results
+        cudaMemcpy(&sum, gpu_sum, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&new_runtime, gpu_runtime, sizeof(double), cudaMemcpyDeviceToHost);
+
+        cudaEventElapsedTime(&msec, start, stop);
+        runtime = msec / 1000.f;
+
+        if (runtime > args->min_runtime)
+            *(args->runtime_flag) = 1;
+
+        // TODO: Set mutex before write?
+
+        // TODO: barrier?
+
+        if (! *(args->runtime_flag)) r_max *= 2;
+    } while (! *(args->runtime_flag));
+
+    args->runtime = runtime;
+    args->flops = (float) NBLOCKS * NTHREADS * NCORES * r_max / runtime;
+    args->bw_load = 0;
+    args->bw_store = 0;
+
+    cudaFree(gpu_sum);
+    cudaFree(gpu_runtime);
+}
