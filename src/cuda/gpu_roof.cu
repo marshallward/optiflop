341699c6e406a64973e34c0ba4ad768068b3c2e0
Marshall Ward <marshall.ward@gmail.com>
2022-01-21 11:26:05 -0500

    Mem-bound GPU ax+y loop
    This tweaks the "gpy_axpy" test to allow multiple calculations per
    thread, and a bit more control (and automation) for thread/block
    decomposition.
    
    I find that for very high array sizes, the best performance happens with
    2 cores per thread (?) and 800 threads per block.  This divides the
    problem into N/1600 blocks.  (Overflow is handled by truncating the
    do-loop in the kernel.  I guess many get truncated to zero iterations?)
    
    Also some of the GPU-local timer stuff print was turned off.  Not sure
    whether to pursue that.  Numbers are great, but I think I am just
    reading one of the warps, and not some synethisized result (e.g. max
    over all warps/blocks/etc).
    
    Also some skeletal code to do timings on the GPU, rather than around the
    kernel submissions.  But that is not quite ready.

--- src/cuda/gpu_roof.cu
+++ src/cuda/gpu_roof.cu
@@ -1,11 +1,14 @@
 #include "roof.h"
+#include <stdio.h>
 
-#define BLOCKSIZE 1024
+#define MAXCORES 2
+#define MAXTHREADS 800
+
 
 __global__ void saxpy(int n, float a, float *x, float *y)
 {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < n)
+    int i0 = MAXCORES * (blockDim.x * blockIdx.x + threadIdx.x);
+    for (int i = i0; i < min(i0 + MAXCORES, n); i++)
         y[i] = a * x[i] + y[i];
 }
 
@@ -18,9 +21,12 @@ void gpu_axpy(int n, float a, float b, float * x_in, float * y_in,
     size_t nbytes;
 
     int r, r_max;
+    int nthreads, nblocks;
 
     cudaEvent_t start, stop;
     float msec, sec;
+
+    volatile double sum;
 
     // Timer setup
     cudaEventCreate(&start);
@@ -33,14 +39,31 @@ void gpu_axpy(int n, float a, float b, float * x_in, float * y_in,
     cudaMemcpy(x, x_in, nbytes, cudaMemcpyHostToDevice);
     cudaMemcpy(y, y_in, nbytes, cudaMemcpyHostToDevice);
 
+    nthreads = min(1 + (n - 1) / MAXCORES, MAXTHREADS);
+    nblocks = 1 + (n - 1) / (MAXTHREADS * MAXCORES);
+
+    //printf("  ncores: %i\n", MAXCORES);
+    //printf("nthreads: %i\n", nthreads);
+    //printf(" nblocks: %i\n", nblocks);
+
     r_max = 1;
     cudaEventRecord(start);
     for (r = 0; r < r_max; r++) {
-        saxpy<<<1 + n / BLOCKSIZE, BLOCKSIZE>>>(n, a, x, y);
+        saxpy<<<nblocks,nthreads>>>(n, a, x, y);
     }
     cudaEventRecord(stop);
 
     cudaMemcpy(y_in, y, nbytes, cudaMemcpyDeviceToHost);
+
+    /* Not yet confident this is working, so check the sum. */
+    /* Also ensures that the value is touched and won't be optimized out. */
+    /* TODO: Later, we can rely on `volatile` and drop this sum. */
+    sum = 0.;
+    for (int i = 0; i < n; i++) sum += y_in[i];
+    if (sum != 4. * n) {
+        printf("ERROR: Sum (%f\n does not match!\n", sum);
+        exit(1);
+    }
 
     cudaFree(x);
     cudaFree(y);
--- src/cuda/gpu_simd.cu
+++ src/cuda/gpu_simd.cu
@@ -5,19 +5,26 @@
 #define NBLOCKS 160
 #define NTHREADS 128
 
-__global__ void kadd(long r_max, float *sum)
+
+__global__ void kadd(long r_max, float *sum, double *runtime)
 {
     const float eps = 1e-6f;
     float reg[NCORES];
     long r;
     int i;
 
+    long long int start, end;
+
     for (i = 0; i < NCORES; i++)
         reg[i] = 1.f;
 
+    start = clock64();
     for (r = 0; r < r_max; r++)
         for (i = 0; i < NCORES; i++)
             reg[i] = reg[i] + eps;
+    end = clock64();
+
+    *runtime = (double) (end - start) / 1.230e9;
 
     *sum = 0.f;
     for (i = 0; i < NCORES; i++) *sum = *sum + reg[i];
@@ -72,10 +79,18 @@ void gpu_add(void *args_in)
     float sum, *gpu_sum;
     float msec, runtime;
 
+    // testing
+    double *gpu_runtime;
+    double new_runtime;
+
     args = (struct roof_args *) args_in;
+
+    // TODO: useful?
+    //cudaDeviceReset();
 
     r_max = 1;
     cudaMalloc(&gpu_sum, sizeof(float));
+    cudaMalloc(&gpu_runtime, sizeof(double));
 
     /* TODO: Move timer to kernel and use clock64() */
     cudaEventCreate(&start);
@@ -84,17 +99,19 @@ void gpu_add(void *args_in)
     *(args->runtime_flag) = 0;
     do {
         cudaEventRecord(start);
-        kadd<<<NBLOCKS,NTHREADS>>>(r_max, gpu_sum);
+        kadd<<<NBLOCKS,NTHREADS>>>(r_max, gpu_sum, gpu_runtime);
         cudaEventRecord(stop);
 
         // Get results
         cudaMemcpy(&sum, gpu_sum, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&new_runtime, gpu_runtime, sizeof(double), cudaMemcpyDeviceToHost);
 
         cudaEventElapsedTime(&msec, start, stop);
         runtime = msec / 1000.f;
 
         if (runtime > args->min_runtime)
             *(args->runtime_flag) = 1;
+
         // TODO: Set mutex before write?
 
         // TODO: barrier?
@@ -104,9 +121,9 @@ void gpu_add(void *args_in)
 
     args->runtime = runtime;
     args->flops = (float) NBLOCKS * NTHREADS * NCORES * r_max / runtime;
-    //args->flops = (float) 2 * NBLOCKS * NTHREADS * NCORES * r_max / runtime;
     args->bw_load = 0;
     args->bw_store = 0;
 
     cudaFree(gpu_sum);
+    cudaFree(gpu_runtime);
 }
