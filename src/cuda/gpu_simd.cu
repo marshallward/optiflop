bd605e064b3140db34273da365cc8f3f018a4500
Marshall Ward <marshall.ward@gmail.com>
2022-01-21 11:26:05 -0500

    Testing out a clock() implementation
    Trying to measure runtime on the GPU rather than outside the kernel.
    
    DOn't really know what's going on here because each thread should have
    its own measurement, but the numbers do seem to be less noisy.
    
    (Maybe I am measuring a random thread?)
    
    Also need some reliable way to get the clock speed.  Apparently it is
    "sometimes but not always" the shader frequency.

--- src/cuda/gpu_simd.cu
+++ src/cuda/gpu_simd.cu
@@ -5,19 +5,28 @@
 #define NBLOCKS 160
 #define NTHREADS 128
 
-__global__ void kadd(long r_max, float *sum)
+// test
+#include <stdio.h>
+
+__global__ void kadd(long r_max, float *sum, double *runtime)
 {
     const float eps = 1e-6f;
     float reg[NCORES];
     long r;
     int i;
 
+    clock_t start, end;
+
     for (i = 0; i < NCORES; i++)
         reg[i] = 1.f;
 
+    start = clock();
     for (r = 0; r < r_max; r++)
         for (i = 0; i < NCORES; i++)
             reg[i] = reg[i] + eps;
+    end = clock();
+
+    *runtime = (double) (end - start) / 1.230e9;
 
     *sum = 0.f;
     for (i = 0; i < NCORES; i++) *sum = *sum + reg[i];
@@ -71,30 +80,42 @@ void gpu_add(void *args_in)
     long r_max;
     float sum, *gpu_sum;
     float msec, runtime;
+    // testing
+    double *gpu_runtime;
+    double new_runtime;
 
     args = (struct roof_args *) args_in;
 
     r_max = 1;
     cudaMalloc(&gpu_sum, sizeof(float));
+    cudaMalloc(&gpu_runtime, sizeof(double));
 
     /* TODO: Move timer to kernel and use clock64() */
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
+    printf("CLOCKS_PER_SEC: %i\n", CLOCKS_PER_SEC);
+
     *(args->runtime_flag) = 0;
     do {
         cudaEventRecord(start);
-        kadd<<<NBLOCKS,NTHREADS>>>(r_max, gpu_sum);
+        kadd<<<NBLOCKS,NTHREADS>>>(r_max, gpu_sum, gpu_runtime);
         cudaEventRecord(stop);
 
         // Get results
         cudaMemcpy(&sum, gpu_sum, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&new_runtime, gpu_runtime, sizeof(double), cudaMemcpyDeviceToHost);
 
         cudaEventElapsedTime(&msec, start, stop);
         runtime = msec / 1000.f;
 
+        // Check the new timer
+        printf("CPU runtime: %.12f\n", runtime);
+        printf("GPU runtime: %.12f\n", new_runtime);
+
         if (runtime > args->min_runtime)
             *(args->runtime_flag) = 1;
+
         // TODO: Set mutex before write?
 
         // TODO: barrier?
@@ -109,4 +130,5 @@ void gpu_add(void *args_in)
     args->bw_store = 0;
 
     cudaFree(gpu_sum);
+    cudaFree(gpu_runtime);
 }
