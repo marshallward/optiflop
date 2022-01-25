98a6bfd5aab3e9995f6a047ea3a6553d7b0ef412
Marshall Ward <marshall.ward@gmail.com>
2022-01-25 12:09:04 -0500

    Some GPU build fixes
    Fixed a bug due to missing generic gpu_fma function.
    
    Also cleaned up some configure.ac issues when NVCC is missing.

--- src/generic/gpu_simd.c
+++ src/generic/gpu_simd.c
@@ -11,3 +11,15 @@ void gpu_add(void *args_in)
     args->bw_load = 0.;
     args->bw_store = 0.;
 }
+
+
+void gpu_fma(void *args_in)
+{
+    struct roof_args *args;
+    args = (struct roof_args *) args_in;
+
+    args->runtime = 0.;
+    args->flops = 0.;
+    args->bw_load = 0.;
+    args->bw_store = 0.;
+}
